555a9bbfd90f73027f96fe6cba17d5b17f8897bb
Dani Youn <semi159753@naver.com>
2023-10-06 18:23:17 -0500

    update .env to add port

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 // set express app
 const app = express();
 
+const port = process.env.PORT;
+
 // connect to the database
 // only listen to incoming requests when database connection is successful
 // return errors if unsuccessful
@@ -13,8 +15,6 @@ mongoose.connect(process.env.DB_CONNECTION_STRING)
         console.log(`server running on port ${port}`);
     }))
     .catch((err) => console.log(err));
-
-const port = 8080;
 
 app.get("/", (req, res) => {
     res.json({"version": "V1"});
