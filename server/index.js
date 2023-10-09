3bc3079b480daf3866fbeef39995f8a79e344dee
Dani Youn <semi159753@naver.com>
2023-10-08 19:50:43 -0500

    delete .env

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT;
 // connect to the database
 // only listen to incoming requests when database connection is successful
 // return errors if unsuccessful
-mongoose.connect(process.env.DB_CONNECTION_STRING)
+mongoose.connect(process.env.MONGODB_URL_DEV)
     .then((result) => app.listen(port, () => {
         console.log(`server running on port ${port}`);
     }))
