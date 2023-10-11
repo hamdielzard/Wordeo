4c4a059c698cd674140c7c06aed60e3f4e298c59
Dani Youn <semi159753@naver.com>
2023-10-11 13:43:37 -0500

    update reference to mongodb connection

--- server/__tests__/test.js
+++ server/__tests__/test.js
@@ -2,9 +2,11 @@ const server = require("../server")
 const supertest = require("supertest");
 const mongoose = require("mongoose");
 require("dotenv").config();
-  
+
+const mongourl = process.env.MONGODB_URL_TEST || process.env.npm_config_mongourl; // fetch from .env or argument (for pipelines)
+
 beforeEach(async () => {
-    await mongoose.connect(process.env.MONGODB_URL_TEST);
+    await mongoose.connect(mongourl);
 });
 
 afterEach(async () => {
