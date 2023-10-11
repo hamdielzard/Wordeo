eea6bc10089f5b206d6f4ea56889686e244236e2
Dani Youn <semi159753@naver.com>
2023-10-10 19:19:40 -0500

    Update dotenv variable name

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT;
 // connect to the database
 // only listen to incoming requests when database connection is successful
 // return errors if unsuccessful
-mongoose.connect(process.env.MONGODB_URL_DEV)
+mongoose.connect(process.env.MONGODB_URL)
     .then((result) => app.listen(port, () => {
         console.log(`server running on port ${port}`);
     }))
