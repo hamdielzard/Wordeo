dd22f234b7d6927050b45a2bef7ad1843bf8b757
Hamdi <mail@hamdielzard.xyz>
2023-10-11 14:22:31 -0500

    Change test connections and fix address

--- server/__tests__/test.js
+++ server/__tests__/test.js
@@ -2,9 +2,15 @@ const server = require("../server")
 const supertest = require("supertest");
 const mongoose = require("mongoose");
 require("dotenv").config();
+
+const username = process.env.MONGODB_USER
+const password = process.env.MONGODB_PASS
+const URL = process.env.MONGOURL
+
+const address = `mongodb+srv://${username}:${password}@${URL}/?retryWrites=true&w=majority`
   
 beforeEach(async () => {
-    await mongoose.connect(process.env.MONGODB_URL_TEST);
+    await mongoose.connect(address);
 });
 
 afterEach(async () => {
--- server/__tests__/words.test.js
+++ server/__tests__/words.test.js
@@ -4,8 +4,14 @@ const supertest = require("supertest");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const username = process.env.MONGODB_USER
+const password = process.env.MONGODB_PASS
+const URL = process.env.MONGOURL
+
+const address = `mongodb+srv://${username}:${password}@${URL}/?retryWrites=true&w=majority`
+
 beforeEach(async () => {
-    await mongoose.connect(process.env.MONGODB_URL_TEST);
+    await mongoose.connect(address);
 
     // seed some fake data
     const testWord = new Word({ word: "test", hints: ["test1", "test2"] });
