f07279b974c7db6807d1feba23d23448b87cda95
Dani Youn <semi159753@naver.com>
2023-10-16 22:05:00 -0500

    add scores endpoints and tests

---
+++ server/__tests__/scores.test.js
@@ -0,0 +1,105 @@
+const server = require("../server");
+const Score = require("../models/scores");
+const { Account } = require("../models/accounts");
+const supertest = require("supertest");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const address = process.env.MONGODB_URL_TEST || `mongodb://localhost:27017/test?directConnection=true&serverSelectionTimeoutMS=2000`;
+
+var testAccountID;
+
+beforeEach(async () => {
+    await mongoose.connect(address);
+
+    // seed some fake data
+    const testAccount = new Account({ user_id: 'user1', user_name: 'test1', password: 'pass' });
+    const accountDoc = await testAccount.save();
+    testAccountID = accountDoc._id;
+    const testScore = new Score({ score: 999, user: testAccountID });
+    await testScore.save();
+});
+
+afterEach(async () => {
+    if (mongoose.connection.readyState) {
+        // delete all populated data
+        await Score.deleteMany({});
+        await Account.deleteMany({});
+        await mongoose.connection.close();
+    }
+});
+
+describe('POST /scores', () => {
+    it('on success, should return the created result with an http status 200', async () => {
+        const payload = { score: 111, userID: testAccountID };
+        const res = await supertest(server)
+            .post('/scores')
+            .send(payload)
+            .set('Content-Type', 'application/json');
+        expect(res.status).toEqual(200);
+        expect(res.body.score).toEqual(payload.score);
+        expect(res.body.user).toEqual(String(payload.userID));
+    });
+
+    it('on fail due to a database error, should return an http status 500', async () => {
+        await mongoose.connection.close();
+        const res = await supertest(server)
+            .post('/scores')
+            .set('Content-Type', 'application/json');
+        expect(res.status).toEqual(500);
+    });
+
+    it('on no user matched, should return an http status 404', async () => {
+        // delete the existing account
+        await Account.deleteOne({ _id:  testAccountID});
+        const payload = { score: 999, userID: testAccountID };
+        const res = await supertest(server)
+            .post('/scores')
+            .send(payload)
+            .set('Content-Type', 'application/json');
+        expect(res.status).toEqual(404);
+    });
+});
+
+describe('GET /scores', () => {
+    it('should return the list of all words', async () => {
+        // add a new score
+        const newScore = new Score({ score: 999, user: testAccountID});
+        await newScore.save();
+        const res = await supertest(server)
+            .get('/scores');
+        expect(res.status).toEqual(200);
+        expect(res.body).toHaveLength(2);
+    });
+
+    it('should filter by the user id when given a query parameter', async () => {
+        // add a new score with a different user
+        const testAccount2 = new Account({ user_id: 'user2', user_name: 'test2', password: 'pass' });
+        const accountDoc = await testAccount2.save();
+        const newScore = new Score({ score: 111, user: accountDoc._id });
+        await newScore.save();
+        const res = await supertest(server)
+            .get(`/scores?userID=${ accountDoc._id }`);
+        expect(res.status).toEqual(200);
+        expect(res.body).toHaveLength(1);
+    });
+
+    it('should limit the number of data when given a query parameter', async () => {
+        // add a new score
+        const accountDoc = await Account.findOne();
+        const newScore = new Score({ score: 999, user: testAccountID });
+        await newScore.save();
+        const res = await supertest(server)
+            .get('/scores?count=1');
+        expect(res.status).toEqual(200);
+        expect(res.body).toHaveLength(1);
+    });
+
+    it('on failure due to a database error, should return an http status 500', async () => {
+        // close db connection to test bad path
+        await mongoose.connection.close();
+        const res = await supertest(server)
+            .get('/scores');
+        expect(res.status).toEqual(500);
+    });
+});
--- server/routes/scores.js
+++ server/routes/scores.js
@@ -3,8 +3,56 @@ const router = express.Router();
 
 // load models
 const Score = require('../models/scores'); 
+const { Account } = require("../models/accounts"); // TODO: match the updated acount (or user) model
 
 // endpoints
 
+// create a new record of a score
+router.post("/", async (req, res) => {
+    const score = req.body.score;
+    const userID = req.body.userID;
+
+    try {
+        const user = await Account.exists({ _id: userID });
+        const newScore = new Score({ 
+            score: score,
+            user: userID
+        });
+
+        if (user) {
+            const result = await newScore.save();
+            res.status(200).json(result);
+        } else {
+            res.status(404).json({ message: `no user with given id: ${userID} was found`});
+        }
+        
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// get all scores
+router.get("/", async (req, res) => {
+    var filter = {};
+
+    // filter by user
+    if (req.query.userID) {
+        filter.user = req.query.userID;
+    }
+
+    try {
+        var result;
+        
+        // return a list of scores in a descending order
+        if (req.query.count) {
+            result = await Score.find(filter).sort({ score: 'desc' }).limit(parseInt(req.query.count));
+        } else {
+            result = await Score.find(filter).sort({ score: 'desc' });
+        }
+        res.status(200).json(result);
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+});
 // export
 module.exports = router;
