a365615ee1483d425dc233b0abd0f733cf033602
Souvik <41978993+raysofhopes@users.noreply.github.com>
2023-10-19 12:38:46 -0500

    Add accounts and points (#76)
    * Added auth api and created auth controller
    
    * Completed registration and login apis and setup testing
    
    * Added auth api and created auth controller
    
    * Completed registration and login apis and setup testing
    
    * fix conflicts
    
    * Delete app.js
    
    ---------
    
    Co-authored-by: Dani Youn <semi159753@naver.com>

--- server/__tests__/test.js
+++ server/__tests__/test.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const address = process.env.MONGODB_URL_TEST || `mongodb://localhost:27017/test?directConnection=true&serverSelectionTimeoutMS=2000`;
-  
+
 beforeEach(async () => {
     await mongoose.connect(address);
 });
@@ -13,10 +13,10 @@ afterEach(async () => {
     await mongoose.connection.close();
 });
 
-describe('test endpoint', () => {
+describe('test version', () => {
     it('should return the version information', async () => {
         const res = await supertest(server).get('/');
         expect(res.status).toEqual(200);
-        expect(res.body).toEqual({"version": "V1"});
+        expect(res.body).toEqual({ "version": "V1" });
     });
 });
