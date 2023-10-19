9f4d9b808587d65b5f68acb4c385b852ec3bcf3c
raysofhopes <41978993+raysofhopes@users.noreply.github.com>
2023-10-19 14:44:51 -0500

    Backend apis and tests working

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,93 +1,81 @@
-const User = require('../models/User')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 // Registration process
 const register = (req, res, next) => {
-    var userName = req.body.userName
+    var userName = req.body.userName;
 
-    User.findOne({userName: userName})
-    .then(user => {
-        if(user)
-        {
-            res.json({
-                message: "User already exists!"
-            })
-        } else {
-            bcrypt.hash(req.body.password, 10, function(err, hashedPass){
-                if(err)
-                {
-                    res.json({
-                        error: err
-                    })
-                }
+    User.findOne({ userName: userName })
+        .then(user => {
+            if (user) {
+                return res.json({
+                    message: "User already exists!"
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, function (err, hashedPass) {
+                    if (err) {
+                        return res.json({
+                            error: err
+                        });
+                    }
 
-                let user = new User({
-                    userName: req.body.userName,
-                    password: hashedPass
-                })
-            
-                user.save()
-                .then(user =>{
-                    res.json({
-                        message: 'User Added Successfully!'
-                    })
-                })
-            
-                .catch(error => {
-                    res.json({
-                        message: 'An error occured!'
-                    })
-                })
-            })
-        }
-    });
+                    let user = new User({
+                        userName: req.body.userName,
+                        password: hashedPass
+                    });
 
-    
-
-
+                    user.save()
+                        .then(user => {
+                            return res.json({
+                                message: 'User Added Successfully!'
+                            });
+                        })
+                        .catch(error => {
+                            return res.json({
+                                message: 'An error occurred!'
+                            });
+                        });
+                });
+            }
+        });
 }
 
 // Login process
 const login = (req, res, next) => {
-    var userName = req.body.userName
-    var password = req.body.password
+    var userName = req.body.userName;
+    var password = req.body.password;
 
-    User.findOne({userName: userName})
-    .then(user => {
-        if(user)
-        {
-            bcrypt.compare(password, user.password, function(err,result){
-                if(err)
-                {
-                    res.json({
+    User.findOne({ userName: userName })
+        .then(user => {
+            if (!user) {
+                return res.json({
+                    message: "No such user found!"
+                });
+            }
+
+            bcrypt.compare(password, user.password, function (err, result) {
+                if (err) {
+                    return res.json({
                         error: err
-                    })
+                    });
                 }
-                if(result){
-                    let token = jwt.sign({userName: user.userName}, 'verySecretValue', {expiresIn: '1h'})
-                    res.json({
+                if (result) {
+                    let token = jwt.sign({ userName: user.userName }, 'verySecretValue', { expiresIn: '1h' });
+                    return res.json({
                         message: 'Login successful!',
                         token
-                    })
-                }
-                else
-                {
-                    res.json({
+                    });
+                } else {
+                    return res.json({
                         message: 'Password does not match!'
-                    })
+                    });
                 }
-            })
-        }
-        else
-        {
-            res.json({
-                message: "No such user found!"
-            })
-        }
-    })
+            });
+        });
 }
 
 module.exports = {
-    register, login
-}
+    register,
+    login
+};
