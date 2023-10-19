5655930665ba5d3ea0123fbaf9c08e2e2f3bba33
Alyssa G <145222737+c4ke2@users.noreply.github.com>
2023-10-19 10:05:19 -0500

    Create cookies and fix visual glitches (#75)
    * add cookies, fix visual errors
    
    * update home.test.js
    
    * fix 'key' error, unused buttons, wrong redirect

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import { useLayoutEffect } from 'react';
 import '../Styles/Home.css';
 
 // Components
@@ -7,28 +7,44 @@ import Button from '../Components/Button';
 import WordeoLogo from '../Images/WordeoLogo.png';
 
 function Home() {
+
+    useLayoutEffect(()=>{
+        document.body.style.backgroundColor = "#FFF";
+    })
+
     return (
         <div className='homepage'>
             <div className="homepageHeader">
                 <img src={WordeoLogo} alt="Wordeo Logo" />
                 <div className="signInButton">
-                <Button label="Not signed in" onClick={() => { window.location = "/account/signin" }} type="secondary" size="medium" />
+                    {loginButton()}
                 </div>
             </div>
             <div className='homepageInteractive'>
-                <Button label="PLAY" onClick={() => { window.location = "/game" }} type="primary" size="large" />       
-                <Button label="Leaderboards" onClick={() => { }} type="primary" size="medium" />
-                <div className='musicSFXToggle'>
-                    <Button label="Music" onClick={() => { }} type="toggles" size="medium" />
-                    <Button label="SFX" onClick={() => { }} type="toggles" size="medium" />
-                </div>
-
+                <Button label="PLAY" onClick={() => { window.location = "/game" }} type="primary" size="large" />
             </div>
             <div className="homepageFooter">
-                Login to save your progress and earn coins!
+                Login to save your progress!
             </div>
         </div>
     );
+}
+
+function loginButton()
+{
+    if((document.cookie.split(";").some((item) => item.trim().startsWith("user="))))
+    {
+        const cookie = ('; '+document.cookie).split(`; user=`).pop().split(';')[0];
+        return(
+                <Button label={cookie} onClick={() => { window.location = "/account/"+cookie }} type="secondary" size="medium" />
+        )
+    }
+    else
+    {
+        return(
+                <Button label="Not signed in" onClick={() => { window.location = "/account/signin" }} type="secondary" size="medium" />
+        )
+    }
 }
 
 function Play() {
