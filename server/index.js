e6bc0ccdce9f8810e5141b203310f76d7a4b63ba
Dani Youn <semi159753@naver.com>
2023-10-24 17:02:29 -0500

    update host & port for server

--- server/index.js
+++ server/index.js
@@ -2,17 +2,18 @@ require('dotenv').config();
 const logger = require('./logger');
 const app = require("./server");
 const mongoose = require('mongoose');
-const data = require('../data/words.json');
+const data = require('./data/words.json');
 const Word = require('./models/words');
 
-const port = process.env.PORT || 8080;
+const HOST = '0.0.0.0';
+const PORT = process.env.PORT || 8080;
 
 // connect to the database
 // only listen to incoming requests when database connection is successful
 // return errors if unsuccessful
 mongoose.connect(process.env.MONGODB_URL)
-    .then((result) => app.listen(port, () => {
-        logger.info(`Server listening on port ${port}`);
+    .then((result) => app.listen(PORT, HOST, () => {
+        logger.info(`Server listening on port ${HOST}:${PORT}`);
     }))
     .then(async () => {
         // clear & insert the word data to the database
