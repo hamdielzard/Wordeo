d28afd392c5666709b0be3a9f95ff553cf2743dd
benedict <agupitab@myumanitoba.ca>
2023-11-04 01:23:20 -0500

    UI for store implemented

---
+++ client/src/TEMPDB/itemdata.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        "name": "Add Time",
+        "description": "Add 5 seconds to the timer for the current word",
+        "category": "powerup",
+        "price": 500,
+    },
+    {
+        "name": "Reveal Letter",
+        "description": "Reveal one random letter in the word",
+        "category": "powerup",
+        "price": 1000
+    },
+    {
+        "name": "Royal",
+        "description": "",
+        "category": "icon",
+        "price": 2500
+    },    
+    {
+        "name": "Halloween",
+        "description": "Reveal one random letter in the word",
+        "category": "icon",
+        "price": 2500
+    },
+]
