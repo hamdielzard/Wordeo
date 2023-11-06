9f8a6d5b6c039af7900960627c65f58a46f36a9d
Dani Youn <semi159753@naver.com>
2023-11-06 08:31:12 -0600

    resolve conflict and format file

--- server/routes/scores.js
+++ server/routes/scores.js
@@ -8,197 +8,154 @@ const logger = require('../logger');
 
 // POST /scores - Create a new record of a score
 router.post("/", async (req, res) => {
-    const score = req.body.score;
-    const userName = req.body.userName;
-    const mode = req.body.gameMode;
+  const score = req.body.score;
+  const userName = req.body.userName;
+  const mode = req.body.gameMode;
 
-    // Validate backend score - TESTED
-    if (score == null || score == undefined || score == "") {
-        res.status(400).json({ message: `No score was provided` });
-        logger.error(`[400] POST /scores - Add score error occurred: no score was provided`);
-        return;
-    }
-    else if (isNaN(score)) {
-        res.status(400).json({ message: `The provided score: ${score} is not a number!` });
-        logger.error(`[400] POST /scores - Add score error occurred: ${score} is not a valid score`);
-        return;
-    }
-    else if (score <= 0) {
-        res.status(400).json({ message: `The provided score: ${score} is negative and/or zero!` });
-        logger.error(`[400] POST /scores - Add score error occurred: ${score} is not a valid score`);
-        return;
-    }
+  // Validate backend score - TESTED
+  if (score == null || score == undefined || score == "") {
+    res.status(400).json({ message: `No score was provided` });
+    logger.error(`[400] POST /scores - Add score error occurred: no score was provided`);
+    return;
+  }
+  else if (isNaN(score)) {
+    res.status(400).json({ message: `The provided score: ${score} is not a number!` });
+    logger.error(`[400] POST /scores - Add score error occurred: ${score} is not a valid score`);
+    return;
+  }
+  else if (score <= 0) {
+    res.status(400).json({ message: `The provided score: ${score} is negative and/or zero!` });
+    logger.error(`[400] POST /scores - Add score error occurred: ${score} is not a valid score`);
+    return;
+  }
 
-    // Validate backend userName - TESTED
-    if (userName == null || userName == undefined || userName == "") {
-        res.status(400).json({ message: `No userName was provided` });
-        logger.error(`[400] POST /scores - Add score error occurred: no userName was provided`);
-        return;
-    }
-    else if (!await User.exists({ userName: userName })) {
-        res.status(404).json({ message: `No user with given userName: ${userName} was found` });
-        logger.error(`[404] POST /scores - Add score error occurred: ${userName} was not found`);
-        return;
-    }
+  // Validate backend userName - TESTED
+  if (userName == null || userName == undefined || userName == "") {
+    res.status(400).json({ message: `No userName was provided` });
+    logger.error(`[400] POST /scores - Add score error occurred: no userName was provided`);
+    return;
+  }
+  else if (!await User.exists({ userName: userName })) {
+    res.status(404).json({ message: `No user with given userName: ${userName} was found` });
+    logger.error(`[404] POST /scores - Add score error occurred: ${userName} was not found`);
+    return;
+  }
 
-    // Validate backend mode - TESTED
-    if (mode == null || mode == undefined || mode == "") {
-        res.status(400).json({ message: `No gameMode was provided` });
-        logger.error(`[400] POST /scores - Add score error occurred: no gameMode was provided`);
-        return;
-    }
-    else if (!Object.values(Modes).includes(mode)) {
-        res.status(404).json({ message: `The provided game mode: ${mode} was invalid` });
-        logger.error(`[404] POST /scores - Add score error occurred: ${mode} is not a valid game mode`);
-        return;
-    }
+  // Validate backend mode - TESTED
+  if (mode == null || mode == undefined || mode == "") {
+    res.status(400).json({ message: `No gameMode was provided` });
+    logger.error(`[400] POST /scores - Add score error occurred: no gameMode was provided`);
+    return;
+  }
+  else if (!Object.values(Modes).includes(mode)) {
+    res.status(404).json({ message: `The provided game mode: ${mode} was invalid` });
+    logger.error(`[404] POST /scores - Add score error occurred: ${mode} is not a valid game mode`);
+    return;
+  }
 
-    // All fields validated. Attempt to add the score - TESTED
-    const newScore = new Score({ score: score, userName: userName, gameMode: mode });
-    newScore.save()
-        .then(score => {
-            res.status(200).json({
-                message: 'Score was added successfully!',
-                score: score.score,
-                userName: score.userName,
-                gameMode: score.gameMode
-            })
-            logger.info(`[200] POST /scores - Add score successful`)
-        })
-        .catch(error => {
-            // WARN: Can't test this?
-            res.status(500).json({
-                message: 'An error occured!'
-            })
-            logger.error(`[500] POST /scores - Add score error occurred`)
-            logger.cont(`Details: ${error}`)
-        })
+  // All fields validated. Attempt to add the score - TESTED
+  const newScore = new Score({ score: score, userName: userName, gameMode: mode });
+  newScore.save()
+    .then(score => {
+      res.status(200).json({
+        message: 'Score was added successfully!',
+        score: score.score,
+        userName: score.userName,
+        gameMode: score.gameMode
+      })
+      logger.info(`[200] POST /scores - Add score successful`)
+    })
+    .catch(error => {
+      // WARN: Can't test this?
+      res.status(500).json({
+        message: 'An error occured!'
+      })
+      logger.error(`[500] POST /scores - Add score error occurred`)
+      logger.cont(`Details: ${error}`)
+    })
 });
 
 // GET /scores - Get scores, which includes filtering by game mode, user name, and count
 router.get("/", async (req, res) => {
 
-    let filter = {};
-    let count = req.query.count;
-    let userName = req.query.userName;
-    let gameMode = req.query.gameMode;
+  let filter = {};
+  let count = req.query.count;
+  let userName = req.query.userName;
+  let gameMode = req.query.gameMode;
 
-    // If all 3 fields are empty, return all scores
-    if (gameMode == null && gameMode == undefined && gameMode == "" &&
-        userName == null && userName == undefined && userName == "" &&
-        count == null && count == undefined && count == "") {
-        count = 10; // Default count to 10, to prevent returning large amounts of data
+  // If all 3 fields are empty, return all scores
+  if (gameMode == null && gameMode == undefined && gameMode == "" &&
+    userName == null && userName == undefined && userName == "" &&
+    count == null && count == undefined && count == "") {
+    count = 10; // Default count to 10, to prevent returning large amounts of data
 
-        Score.find(filter)
-            .limit(count)
-            .sort({ score: -1 })
-            .then(response => {
-                res.status(200).json({
-                    response
-                })
-                logger.info(`[200] GET /scores - Get ALL scores successful`)
-            })
-            .catch(error => {
-                res.status(500).json({
-                    message: 'Failed to get all scores!'
-                })
-                logger.error(`[500] GET /scores - Get ALL scores error occurred`)
-                logger.cont(`Details: ${error}`)
-            })
+    Score.find(filter)
+      .limit(count)
+      .sort({ score: -1 })
+      .then(response => {
+        res.status(200).json({
+          response
+        })
+        logger.info(`[200] GET /scores - Get ALL scores successful`)
+      })
+      .catch(error => {
+        res.status(500).json({
+          message: 'Failed to get all scores!'
+        })
+        logger.error(`[500] GET /scores - Get ALL scores error occurred`)
+        logger.cont(`Details: ${error}`)
+      })
+  }
+  // Otherwise, filter by the given fields
+  else {
+    if (gameMode != null && gameMode != undefined && gameMode != "") {
+      filter.gameMode = gameMode;
+      // Check if the game mode is part of the enum
+      if (!Object.values(Modes).includes(gameMode)) {
+        // 400
+        res.status(400).json({ message: `The provided game mode: ${gameMode} was invalid` });
+        logger.error(`[400] GET /scores - SCORES: Get filtered scores error occurred: ${gameMode} is not a valid game mode`);
+        return;
+      }
     }
-    // Otherwise, filter by the given fields
+    if (userName != null && userName != undefined && userName != "") {
+      filter.userName = userName;
+      // Check if the user exists
+      const user = await User.exists({ userName: userName });
+      if (!user) {
+        // 404
+        res.status(404).json({ message: `No user with given userName: ${userName} was found` });
+        logger.error(`[404] GET /scores - SCORES: Get filtered scores error occurred: ${userName} was not found`);
+        return;
+      }
+    }
+    if (count != null && count != undefined && count != "") {
+      count = parseInt(count);
+    }
     else {
-        if (gameMode != null && gameMode != undefined && gameMode != "") {
-            filter.gameMode = gameMode;
-            // Check if the game mode is part of the enum
-            if (!Object.values(Modes).includes(gameMode)) {
-                // 400
-                res.status(400).json({ message: `The provided game mode: ${gameMode} was invalid` });
-                logger.error(`[400] GET /scores - SCORES: Get filtered scores error occurred: ${gameMode} is not a valid game mode`);
-                return;
-            }
-        }
-        if (userName != null && userName != undefined && userName != "") {
-            filter.userName = userName;
-            // Check if the user exists
-            const user = await User.exists({ userName: userName });
-            if (!user) {
-                // 404
-                res.status(404).json({ message: `No user with given userName: ${userName} was found` });
-                logger.error(`[404] GET /scores - SCORES: Get filtered scores error occurred: ${userName} was not found`);
-                return;
-            }
-        }
-        if (count != null && count != undefined && count != "") {
-            count = parseInt(count);
-        }
-        else {
-            count = 10; // Default count to 10, to prevent returning large amounts of data
-        }
-        Score.find(filter)
-            .limit(count)
-            .sort({ score: -1 })
-            .then(response => {
-                // 200 
-                res.status(200).json({
-                    response
-                })
-                logger.info(`[200] GET /scores - SCORES: Get filtered scores successful`)
-            })
-            .catch(error => {
-                res.status(500).json({
-                    message: 'Failed to get scores!'
-                })
-                logger.error(`[500] GET /scores - SCORES: Get filtered scores has failed`)
-                logger.cont(`Details: ${error}`)
-            })
-
+      count = 10; // Default count to 10, to prevent returning large amounts of data
     }
+    Score.find(filter)
+      .limit(count)
+      .sort({ score: -1 })
+      .then(response => {
+        // 200 
+        res.status(200).json({
+          response
+        })
+        logger.info(`[200] GET /scores - SCORES: Get filtered scores successful`)
+      })
+      .catch(error => {
+        res.status(500).json({
+          message: 'Failed to get scores!'
+        })
+        logger.error(`[500] GET /scores - SCORES: Get filtered scores has failed`)
+        logger.cont(`Details: ${error}`)
+      })
+
+  }
 
 });
 
-    try {
-      const leaderboard = await Score.aggregate([
-        {
-          $match: {
-            gameMode: gameMode || Modes.Solo // if nothing matched, get solo mode leaderboard
-          }
-        },
-        {
-          $group: {
-            _id: "$user",
-            highestScore: { $max: "$score" }
-          }
-        },
-        {
-          $lookup: {
-            from: "users", 
-            localField: "_id",
-            foreignField: "_id",
-            as: "userAccount"
-          }
-        },
-        {
-          $unwind: "$userAccount"
-        },
-        {
-          $project: {
-            _id: 1,
-            highestScore: 1,
-            displayName: "$userAccount.displayName",
-            userName: "$userAccount.userName"
-          }
-        },
-        {
-          $sort: { highestScore: -1 }
-        }
-      ]);
-      res.status(200).json(leaderboard);
-      logger.info(`[200] GET /scores/leaderboard - Get leaderboard successful for gameMode: ${gameMode}`);
-    } catch (err) {
-      res.status(500).json({ message: err.message })
-      logger.error(`[500] GET /scores/leaderboard - Get leaderboard error occured: ${err}`);
-    }   
-})
 // export
 module.exports = router;
