981356f8273315fce980e2ee2c18cb86bd8faf52
Souvik <41978993+raysofhopes@users.noreply.github.com>
2023-11-10 11:16:41 -0600

    Update server/routes/user.js
    Co-authored-by: Dani Youn <66076626+DaniYoun@users.noreply.github.com>

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,7 +7,7 @@ router.get('/', UserController.index)
 router.patch('/', UserController.update)
 router.patch('/inventory', UserController.createInventory)
 router.patch('/coin', UserController.updateCoins)
-router.get('/coin-balance', UserController.getCoinBalance)
+router.get('/coin', UserController.getCoinBalance)
 router.delete('/', UserController.destroy)
 
 module.exports = router
